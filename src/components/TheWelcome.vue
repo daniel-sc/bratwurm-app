0647606f9bd077724b2dfcf568166ef43c309286
Daniel Schreiber <daniel-schreiber@gmx.de>
2023-01-07 21:10:06 +0100

    remove scaffolded code + github deploy

--- src/components/TheWelcome.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-import DiceDisplay from "@/components/DiceDisplay.vue";</script>
-
-<template>
-  <DiceDisplay />
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import DiceDisplay from "@/components/DiceDisplay.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <DiceDisplay />
   </main>
 </template>
