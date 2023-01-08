e6bfd8593d1fd807b6c32f5037c788c5dbdc8be6
Daniel Schreiber <daniel-schreiber@gmx.de>
2023-01-08 22:18:10 +0100

    feat: full input functional

--- src/components/DiceDisplay.vue
+++ src/components/DiceDisplay.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
 import SingleDice from "@/components/SingleDice.vue";
-import { reactive, ref } from "vue";
+import { computed, reactive } from "vue";
+import { QBtn } from "quasar";
+import { matAdd } from "@quasar/extras/material-icons";
 
 type DiceType = keyof typeof inOutCounts;
-const maxOut = ref(4);
-const maxIn = ref(4);
+const maxDice = 8;
+const showCountOutMin = 4;
+const showCountInMin = 4;
+const showCountOutMax = maxDice - 1;
+const showCountInMax = maxDice - 1;
+const showCountOut = computed(() =>
+  Math.max(...Object.values(inOutCounts).map((x) => x.out), showCountOutMin)
+);
+const showCountIn = computed(() =>
+  Math.max(...Object.values(inOutCounts).map((x) => x.in), showCountInMin)
+);
+const totalDiceCount = computed(() =>
+  Object.values(inOutCounts).reduce((a, b) => a + b.out + b.in, 0)
+);
 const hovered = reactive<{
   inOut: "in" | "out" | null;
   diceNo: number;
   i: number;
 }>({ inOut: null, diceNo: 0, i: 0 });
 const inOutCounts = reactive({
-  1: { in: 1, out: 0 },
-  2: { in: 2, out: 0 },
-  3: { in: 3, out: 1 },
-  4: { in: 0, out: 2 },
+  1: { in: 0, out: 0 },
+  2: { in: 0, out: 0 },
+  3: { in: 0, out: 0 },
+  4: { in: 0, out: 0 },
   5: { in: 0, out: 0 },
   6: { in: 0, out: 0 },
 });
+
 function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
   if (inOutCounts[diceNo][inOut] === i) {
     inOutCounts[diceNo][inOut] = 0;
@@ -28,12 +43,38 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
 }
 </script>
 
+// TODO touch support
+
 <template>
   <div class="grid">
+    <div
+      class="divider"
+      :style="{ gridRow: showCountOutMax + 2, gridColumn: '1 / span 6' }"
+    >
+      Rausgelegt
+      <hr />
+      Wurf
+      <div v-if="totalDiceCount > maxDice" class="warning">
+        {{ totalDiceCount - maxDice }} Würfel zuviel
+      </div>
+      <div v-if="totalDiceCount < maxDice" class="info">
+        {{ maxDice - totalDiceCount }} Würfel fehlen
+      </div>
+    </div>
     <template v-for="(inOut, diceNo) in inOutCounts" :key="diceNo">
+      <q-btn
+        flat
+        round
+        color="dark"
+        :icon="matAdd"
+        :style="{ gridRow: 1, gridColumn: diceNo }"
+        :disable="showCountOut === showCountOutMax"
+        @click="inOutCounts[diceNo]['out'] += 1"
+      />
+
       <SingleDice
         :diceNo="diceNo"
-        v-for="i in maxOut"
+        v-for="i in showCountOut"
         :key="i"
         class="dice"
         :class="{
@@ -43,7 +84,7 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
             hovered.diceNo === diceNo &&
             hovered.i >= i,
         }"
-        :style="{ gridRow: maxOut - i + 1, gridColumn: diceNo }"
+        :style="{ gridRow: showCountOutMax - i + 2, gridColumn: diceNo }"
         @click="clickDice(diceNo as DiceType, 'out', i)"
         @mouseenter="
           hovered.diceNo = diceNo;
@@ -52,13 +93,9 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
         "
         @mouseleave="hovered.inOut = null"
       />
-      <div
-        class="divider"
-        :style="{ gridRow: maxOut + 1, gridColumn: diceNo }"
-      />
       <SingleDice
         :diceNo="diceNo"
-        v-for="i in maxIn"
+        v-for="i in showCountIn"
         :key="i"
         class="dice"
         :class="{
@@ -68,7 +105,7 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
             hovered.diceNo === diceNo &&
             hovered.i >= i,
         }"
-        :style="{ gridRow: maxOut + 1 + i, gridColumn: diceNo }"
+        :style="{ gridRow: showCountOutMax + 2 + i, gridColumn: diceNo }"
         @click="clickDice(diceNo, 'in', i)"
         @mouseenter="
           hovered.diceNo = diceNo;
@@ -76,6 +113,18 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
           hovered.inOut = 'in';
         "
         @mouseleave="hovered.inOut = null"
+      />
+      <q-btn
+        flat
+        round
+        color="dark"
+        :icon="matAdd"
+        :style="{
+          gridRow: showCountOutMax + showCountInMax + 3,
+          gridColumn: diceNo,
+        }"
+        :disable="showCountIn === showCountInMax"
+        @click="inOutCounts[diceNo]['in'] += 1"
       />
     </template>
   </div>
@@ -101,11 +150,26 @@ function clickDice(diceNo: DiceType, inOut: "in" | "out", i: number) {
 }
 
 .dice.hovered {
-  color: var(--hover-color);
+  color: var(--color-hover);
 }
 
 .divider {
-  border-bottom: 2px solid black;
-  margin: var(--dist-small) 0;
+  text-align: center;
+  position: relative;
 }
+
+.warning,
+.info {
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+  background-color: var(--color-background);
+  padding: var(--dist-small);
+}
+
+.warning {
+  color: var(--color-warn);
+}
+
+/* TODO animate dice in/out */
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/de";
+import quasarIconSet from "quasar/icon-set/svg-material-icons";
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
 
 const app = createApp(App);
 
 app.use(router);
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+});
 
 app.mount("#app");
